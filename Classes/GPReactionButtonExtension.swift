c9a73a9fd88ff498a25b2072e3467f1e419670b6
starfall_9000 <starfall.9000.21@gmail.com>
2019-12-10 01:48:37 +0700

    support observe change reaction callback

--- Classes/GPReactionButtonExtension.swift
+++ Classes/GPReactionButtonExtension.swift
@@ -31,7 +31,9 @@ public extension Reactive where Base: ReactionButton {
 public extension ReactionButton {
     // MARK: convenience method
     static func gapo(selectorBackground: UIColor = .white,
-                     selectorList: [Reaction] = Reaction.gapo.all) -> ReactionButton {
+                     selectorList: [Reaction] = Reaction.gapo.all,
+                     likeReaction: Reaction = Reaction.gapo.like,
+                     unlikeReaction: Reaction = Reaction.gapo.unlike) -> ReactionButton {
         let reactionSelector = ReactionSelector()
         reactionSelector.config = ReactionSelectorConfig {
             $0.iconSize = 40
@@ -41,12 +43,34 @@ public extension ReactionButton {
         let reactionButton = ReactionButton()
         reactionButton.reactionSelector = reactionSelector
         reactionButton.config = ReactionButtonConfig() { $0.alignment = .centerLeft }
+        reactionButton.likeReaction = likeReaction
+        reactionButton.unlikeReaction = unlikeReaction
         return reactionButton
     }
     
     static func darkMode() -> ReactionButton {
         return gapo(selectorBackground: UIColor.init(r: 26, g: 26, b: 26, a: 0.7),
-                    selectorList: Reaction.gapo.dark)
+                    selectorList: Reaction.gapo.dark,
+                    unlikeReaction: Reaction.gapo.whiteLike)
+    }
+    
+    func observeReaction() -> Disposable {
+        return rx.reaction.subscribe(onNext: { [weak self] (reaction) in
+            guard let self = self else { return }
+            self.handleChangeReaction(reaction)
+        })
+    }
+    
+    func handleChangeReaction(_ reaction: Reaction?) {
+        if isInitObserve {
+            // handle first time goto this action
+            isInitObserve = false
+            return
+        }
+        if (reaction?.id == unlikeReaction.id) {
+            self.reaction = likeReaction
+        }
+        didUpdateReaction?(self.reaction)
     }
 }
 
